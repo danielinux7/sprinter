36d8d74f7cff4dfb987ac7c9bdc2e9c74593e083
Nart Tlisha <daniel.abzakh@gmail.com>
2021-06-24 14:54:54 +0300

    Update sprint.js

--- sprint.js
+++ sprint.js
@@ -1,20 +1,20 @@
 const github = require("@actions/github");
 const core = require("@actions/core");
 
-var labelsToAdd = core
-  .getInput("add-labels")
-  .split(",")
-  .map(x => x.trim());
+// var labelsToAdd = core
+//   .getInput("add-labels")
+//   .split(",")
+//   .map(x => x.trim());
 
-var labelsToRemove = core
-  .getInput("remove-labels")
-  .split(",")
-  .map(x => x.trim());
+// var labelsToRemove = core
+//   .getInput("remove-labels")
+//   .split(",")
+//   .map(x => x.trim());
 
-async function label() {
+async function sprint() {
   const myToken = core.getInput("repo-token");
-  const ignoreIfAssigned = core.getInput("ignore-if-assigned");
-  const ignoreIfLabeled = core.getInput("ignore-if-labeled");
+//   const ignoreIfAssigned = core.getInput("ignore-if-assigned");
+//   const ignoreIfLabeled = core.getInput("ignore-if-labeled");
   const octokit = new github.GitHub(myToken);
   const context = github.context;
   const repoName = context.payload.repository.name;
@@ -36,9 +36,9 @@ async function label() {
     return "No action being taken. Ignoring because issueNumber was not identified";
   }
 
-  labelsToAdd = labelsToAdd.filter(value => ![""].includes(value));
+//   labelsToAdd = labelsToAdd.filter(value => ![""].includes(value));
 
-  labelsToRemove = labelsToRemove.filter(value => ![""].includes(value));
+//   labelsToRemove = labelsToRemove.filter(value => ![""].includes(value));
 
   // query for the most recent information about the issue. Between the issue being created and
   // the action running, labels or asignees could have been added
@@ -48,38 +48,47 @@ async function label() {
     issue_number: issueNumber
   });
 
-  if (ignoreIfAssigned) {
-    // check if the issue has been assigned to anyone
-    if (updatedIssueInformation.data.assignees.length !== 0) {
-      return "No action being taken. Ignoring because one or more assignees have been added to the issue";
-    }
-  }
+  // If in TODO, INPROGRESS or DONE columns
+//   if (ignoreIfAssigned) {
+//     // check if the issue has a sprint
+//     if (updatedIssueInformation.data.milestone_number !== -1) {
+//       return "No action being taken. Ignoring because one or more assignees have been added to the issue";
+//     }
+//   }
+  
+//   // If in BACKLOG column
+//   else if (ignoreIfAssigned) {
+//     // check if the issue has a sprint
+//     if (updatedIssueInformation.data.milestone_number !== -1) {
+//       return "No action being taken. Ignoring because one or more assignees have been added to the issue";
+//     }
+//   }
 
-  let labels = updatedIssueInformation.data.labels.map(label => label.name);
-  if (ignoreIfLabeled) {
-    if (labels.length !== 0) {
-      return "No action being taken. Ignoring because one or labels have been added to the issue";
-    }
-  }
+//   let labels = updatedIssueInformation.data.labels.map(label => label.name);
+//   if (ignoreIfLabeled) {
+//     if (labels.length !== 0) {
+//       return "No action being taken. Ignoring because one or labels have been added to the issue";
+//     }
+//   }
 
-  for (let labelToAdd of labelsToAdd) {
-    if (!labels.includes(labelToAdd)) {
-      labels.push(labelToAdd);
-    }
-  }
+//   for (let labelToAdd of labelsToAdd) {
+//     if (!labels.includes(labelToAdd)) {
+//       labels.push(labelToAdd);
+//     }
+//   }
 
-  labels = labels.filter(value => !labelsToRemove.includes(value));
-
+//   labels = labels.filter(value => !labelsToRemove.includes(value));
+  sprint = 5
   await octokit.issues.update({
     owner: ownerName,
     repo: repoName,
     issue_number: issueNumber,
-    labels: labels
+    milestone_number: sprint
   });
-  return `Updated labels in ${issueNumber}. Added: ${labelsToAdd}. Removed: ${labelsToRemove}.`;
+  return `Updated sprint in ${issueNumber}. Added: ${sprint}. Removed: ${sprint}.`;
 }
 
-label()
+sprint()
   .then(
     result => {
       // eslint-disable-next-line no-console
